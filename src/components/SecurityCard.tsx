7d93984cefd15e0dcf4821635750c08349fa2e4b
shubhangi-dev-70108 <shubhangi70108@gmail.com>
2026-05-29 15:57:21 +0530

    2FA authentication model update with OTP

--- src/components/SecurityCard.tsx
+++ src/components/SecurityCard.tsx
@@ -8,6 +8,11 @@ import { useState } from "react";
 
 export default function SecurityCard({ themeTextColor, themeBgColor }: any) {
   const [showPasswordModal, setShowPasswordModal] = useState(false);
+  const [show2FAModal, setShow2FAModal] = useState(false);
+  const [phone, setPhone] = useState("");
+  const [otp, setOtp] = useState("");
+  const [otpSent, setOtpSent] = useState(false);
+
   function SecurityItem({
     icon,
     title,
@@ -59,16 +64,16 @@ export default function SecurityCard({ themeTextColor, themeBgColor }: any) {
         onClick={() => setShowPasswordModal(true)}
       />
 
-        {/* password modal */}
+      {/* password modal */}
       {showPasswordModal && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm">
-          <div className="w-full max-w-md rounded-2xl bg-white p-6 shadow-2xl border border-gray-100">
+          <div className={`w-full max-w-md rounded-2xl ${themeBgColor} p-6 shadow-2xl border border-gray-100`}>
 
             <div className="mb-5">
-              <h2 className="text-lg font-semibold text-gray-900">
+              <h2 className={`text-lg font-semibold ${themeTextColor} dark:text-white`}>
                 Change Password
               </h2>
-              <p className="text-sm text-gray-500 mt-1">
+              <p className={`text-sm ${themeTextColor} dark:text-gray-400`} mt-1>
                 Update your account password securely.
               </p>
             </div>
@@ -96,7 +101,7 @@ export default function SecurityCard({ themeTextColor, themeBgColor }: any) {
             <div className="mt-6 flex justify-end gap-3">
               <button
                 onClick={() => setShowPasswordModal(false)}
-                className="rounded-xl border border-gray-200 px-4 py-2 text-sm font-medium text-gray-600 hover:bg-gray-50"
+                className={`rounded-xl border border-gray-200 px-4 py-2 text-sm font-medium ${themeTextColor}`}
               >
                 Cancel
               </button>
@@ -115,7 +120,97 @@ export default function SecurityCard({ themeTextColor, themeBgColor }: any) {
       <SecurityItem
         icon={<KeyRound size={15} className="text-green-600" />}
         title="2FA Authentication"
+        onClick={() => setShow2FAModal(true)}
       />
+
+      {/* 2FA Modal */}
+
+      {show2FAModal && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm">
+          <div className={`w-full max-w-md rounded-2xl ${themeBgColor} dark:bg-slate-900 p-6 shadow-2xl border border-gray-100 dark:border-slate-700`}>
+
+            <h2 className={`text-lg font-semibold ${themeTextColor} dark:text-white`}>
+              Mobile 2FA Verification
+            </h2>
+
+            <p className={`mt-1 text-sm ${themeTextColor} dark:text-gray-400`}>
+              Secure your account using OTP verification.
+            </p>
+
+            {/* Mobile Number */}
+            <div className="mt-5">
+              <label className={`mb-2 block text-sm font-medium ${themeTextColor} dark:text-gray-300`}>
+                Mobile Number
+              </label>
+
+              <input
+                type="tel"
+                value={phone}
+                onChange={(e) => {const value = e.target.value.replace(/[^0-9+]/g, "");
+                  setPhone(value);
+                }}
+                placeholder="+91 "
+                maxLength={13}
+                className="w-full rounded-xl border border-gray-200 dark:border-slate-700
+          bg-gray-50 dark:bg-slate-800
+          px-4 py-3 text-sm
+          text-gray-900 dark:text-white
+          outline-none focus:border-blue-500"
+              />
+            </div>
+
+            {/* OTP */}
+            {otpSent && (
+              <div className="mt-4">
+                <label className="mb-2 block text-sm font-medium text-gray-700 dark:text-gray-300">
+                  Enter OTP
+                </label>
+
+                <input
+                  type="text"
+                  value={otp}
+                  onChange={(e) => setOtp(e.target.value)}
+                  placeholder="6-digit OTP"
+                  maxLength={6}
+                  className="w-full rounded-xl border border-gray-200 dark:border-slate-700
+            bg-gray-50 dark:bg-slate-800
+            px-4 py-3 text-sm
+            text-gray-900 dark:text-white
+            outline-none focus:border-green-500"
+                />
+              </div>
+            )}
+
+            <div className="mt-6 flex justify-end gap-3">
+
+              <button
+                onClick={() => setShow2FAModal(false)}
+                className={`cursor-pointer rounded-xl border border-gray-200 dark:border-slate-700 px-4 py-2 text-sm ${themeTextColor} dark:text-white`}
+              >
+                Cancel
+              </button>
+
+              {!otpSent ? (
+                <button
+                  onClick={() => setOtpSent(true)}
+                  className="cursor-pointer rounded-xl bg-gradient-to-r from-blue-600 to-cyan-500 px-5 py-2 text-sm text-white"
+                >
+                  Send OTP
+                </button>
+              ) : (
+                <button
+                  className="cursor-pointer rounded-xl bg-gradient-to-r from-green-600 to-emerald-500 px-5 py-2 text-sm text-white"
+                >
+                  Verify OTP
+                </button>
+              )}
+
+            </div>
+
+          </div>
+        </div>
+      )}
+
 
     </div>
   );
